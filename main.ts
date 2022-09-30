452b0392ac7a2c92afb06a239231ea323d3c6011
swa07016 <swa07016@khu.ac.kr>
2022-10-01 04:04:47 +0900

    Version 1.0.0

--- main.ts
+++ main.ts
@@ -14,5 +14,20 @@ ScriptApp.onSay.Add(function (player, text) {
     const randomIndex = Math.floor(Math.random() * players.length);
 
     ScriptApp.showCenterLabel(`우리의 조장은 ${players[randomIndex].name}!!`);
+    for(let i=0; i<players.length; i++) {
+      if(i == randomIndex) {
+        player.title = '😂';
+      }  else {
+        player.title = '👏';
+      }
+    }
+
+    setTimeout(function () {
+      const players = ScriptApp.players;
+      players.forEach(function (player) {
+        player.title = '';
+      })
+    }, 2500);
+
   }
 });
