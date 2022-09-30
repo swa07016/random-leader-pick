9a7bab9caebfa3cb2e0c94c0e95dce9a72d1ade6
swa07016 <swa07016@khu.ac.kr>
2022-10-01 03:18:15 +0900

    Feature: 랜덤 조장 뽑기 기능

--- main.ts
+++ main.ts
@@ -4,8 +4,15 @@
 
 import "zep-script";
 
-ScriptApp.onSay.Add(function (player, text) {
-  if(text == 'PLAY') {
+ScriptApp.onJoinPlayer.Add(function () {
+  ScriptApp.showCenterLabel(`조장을 뽑으려면 채팅에 LEADER를 입력하세요!`);
+})
 
+ScriptApp.onSay.Add(function (player, text) {
+  if(text == 'LEADER') {
+    const players = ScriptApp.players;
+    const randomIndex = Math.floor(Math.random() * players.length);
+
+    ScriptApp.showCenterLabel(`우리의 조장은 ${players[randomIndex].name}!!`);
   }
 });
